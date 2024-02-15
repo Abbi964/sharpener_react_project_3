45fd379ca62626defe48651077f79199bcfa4723
abbi964 <abhinavthapliyal964@gmail.com>
2024-02-15 20:01:29 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error , setError] = useState(null);
+  const [fetchMovies, setFetchMovies] = useState(0)
+
+  useEffect(()=>{
+    console.log(fetchMovies)
+    //  continusly fething
+    if (fetchMovies > 0){
+      setTimeout(fetchMoviesHandler,2000)
+    }
+    
+  },[fetchMovies])
 
   async function fetchMoviesHandler(){
-    setIsLoading(true)
-    let response = await fetch('https://swapi.dev/api/films')
-    let responseObj = await response.json()
-    setIsLoading(false)
-    let movies = responseObj.results.map((movie)=>{
-      return {
-        id : movie.episode_id,
-        title : movie.title,
-        openingText : movie.opening_crawl,
-        releseDate : movie.relese_date
-      }
-    })
+    try{
+      setIsLoading(true)
+      let response = await fetch('https://swapi.dev/api/film')
 
-    setMovies(movies)
+      if(!response.ok){
+        throw new Error('Something Went wrong...Retrying')
+      }
+
+      let responseObj = await response.json()
+      setIsLoading(false)
+      let movies = responseObj.results.map((movie)=>{
+        return {
+          id : movie.episode_id,
+          title : movie.title,
+          openingText : movie.opening_crawl,
+          releseDate : movie.relese_date
+        }
+      })
+  
+      setMovies(movies)
+    }
+    catch(err){
+      setError(err.message)
+      setFetchMovies(prev => +prev + 1)
+    }
+    setIsLoading(false)
+  }
+
+  function fetchingStopHandler(){
+    setFetchMovies(-1)
   }
 
   return (
@@ -31,6 +58,7 @@ function App() {
       </section>
       <section>
         <MoviesList movies={movies} />
+        {!isLoading && error && <div><p>{error}</p><button onClick={fetchingStopHandler}>stop</button></div>}
       </section>
     </React.Fragment>
   );
